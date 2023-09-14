29b97c5a0fa8772676bd44c3d8789a47896ca64f
Rajdeep Mandal <116448923+Raj-deepp@users.noreply.github.com>
2023-09-15 00:41:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// const panels = document.querySelectorAll('.panel')
+// const body = document.body;
+
+// panels.forEach(panel => {
+//     removeActiveClasses()
+//     panel.addEventListener('mouseover', () => {
+//         panel.classList.add('active');
+//         // console.log(panel);
+//     })
+// })
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//         body.style.backgroundImage='none'
+//     })
+// }
